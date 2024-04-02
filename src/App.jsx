95b083c890ecd466712c0636a4468cdde6f6b686
João Gabriel Oliveira <30786108+jg-lopes@users.noreply.github.com>
2024-04-02 10:43:08 +0000

    Add border!

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 
+// TODO
+// 1. Unify score across all Fretboards
+// 2. Beautify button
+// 3. Add a common thread generation to avoid creating 2 of the same fret
+// 4. Add a two/three space hit window
+// 5. Add a difficulty slider
+
 function Fret({ active, button = false, playGreen, color }) {
   const buttonColor = active ? color : "gray";
 
-  const buttonStyle = { backgroundColor: buttonColor };
+  const buttonStyle = { backgroundColor: buttonColor,     border: button ? '5px solid gold' : '1px solid' // Add border conditionally
+ };
 
   return (
-    <tr>
-      <td style={buttonStyle}>
-        {button && '==================='}
+    <tr style={buttonStyle}>
+      <td >
+        
       </td>
     </tr>
   );
@@ -24,7 +32,7 @@ function Fretboard({ color }) {
 
   function nextFrame() {
     setFrets((prevData) => {
-      const newFrets = [Math.random() < 0.3, ...prevData.slice(0, -1)];
+      const newFrets = [Math.random() < 0.05, ...prevData.slice(0, -1)];
 
       if (newFrets[newFrets.length - 1] === true) {
         setScore(0);
@@ -90,14 +98,18 @@ function Fretboard({ color }) {
 export default function App() {
   return (
     <main>
+      <div class='introtext'>
+      <h1>Score Hero!</h1>
+        </div>
+      <div class='introtext'>
       <p>Tap anywhere to play the frets!</p>
-
+      </div>
       <div class='game'>
         <Fretboard color="green" />
         <Fretboard color="red" />
-        {/* <Screen color="yellow" /> */}
-        {/* <Screen color="blue" />
-        <Screen color="orange" /> */}
+        <Fretboard color="yellow" />
+        <Fretboard color="blue" />
+        <Fretboard color="orange" />
       </div>
     </main>
   );
